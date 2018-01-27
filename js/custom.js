f9a86983cb9cbef28aae89e5bfdf4a19ed82df74
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-01-27 11:02:07 +0600

    Menu

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+	// SlickNavMobileMenu
+    $(".main-menu ul#main-menu").slicknav({
+        allowParentLinks: true,
+        prependTo:'.sgs-responsive-menu',
+        label: ''
+    });
+
+
+});
