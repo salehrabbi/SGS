ba1bc2f7da86bc15912e656d696fdf4442abe7a2
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-02-16 20:44:57 +0600

    all Done

--- js/custom.js
+++ js/custom.js
@@ -39,37 +39,6 @@ $(document).ready(function () {
     });
 });
 
-
-//Active button of the title================
-
-$('.type-button a').on('click', function(){
-    $('.type-button a.current').removeClass('current');
-    $(this).addClass('current');
-});
-
-//Change the dropdown value===========
-//repitable===
-$(".type-button-link-1").click(function () {
-    $('.video-select-type.current').removeClass('.current');
-    $('.video-select-type').addClass('current');
-    $('.video-select-type.current').val("the_title_value1");
-});
-//repitable===
-$(".type-button-link-2").click(function () {
-    $('.video-select-type.current').removeClass('.current');
-    $('.video-select-type').addClass('current');
-    $('.video-select-type.current').val("the_title_value2");
-});
-
-//repitable===
-$(".type-button-link-3").click(function () {
-    $('.video-select-type.current').removeClass('.current');
-    $('.video-select-type').addClass('current');
-    $('.video-select-type.current').val("the_title_value3");
-});
-
-
-
 //repitable===
 //Text of those ecommerce form=======
 var $title3 = [".title", ".title1", ".title2", ".title3"];
@@ -80,6 +49,38 @@ var $minute3 = [".minute", ".minute1", ".minute2", ".minute3"];
 //repitable===
 var $price3 = [".price", ".price1", ".price2", ".price3"];
 
+var $link = [".item1", ".item2", ".item3"];
+//Active button of the title================
+
+$('.videos-item').on('click', function(){
+    $('.videos-item.current').removeClass('current');
+    $(this).addClass('current');
+});
+
+//Change the dropdown value===========
+//repitable===
+$($link[0]).click(function () {
+    $('.video-select-type.current').removeClass('.current');
+    $('.video-select-type').addClass('current');
+    $('.video-select-type.current').val("the_title_value1");
+});
+//repitable===
+$($link[1]).click(function () {
+    $('.video-select-type.current').removeClass('.current');
+    $('.video-select-type').addClass('current');
+    $('.video-select-type.current').val("the_title_value2");
+});
+
+//repitable===
+$($link[2]).click(function () {
+    $('.video-select-type.current').removeClass('.current');
+    $('.video-select-type').addClass('current');
+    $('.video-select-type.current').val("the_title_value3");
+});
+
+
+
+
 
 
 //change the text by use dropdown =================
@@ -89,27 +90,27 @@ var $price3 = [".price", ".price1", ".price2", ".price3"];
 $('select').on('change', function () {
     var value = $(this).find(':selected').val();
     if (value == 'the_title_value1') {
-        $('.type-button-link-1').addClass("current");
-        $('.type-button-link-2').removeClass("current");
-        $('.type-button-link-3').removeClass("current");
+        $($link[0]).addClass("current");
+        $($link[1]).removeClass("current");
+        $($link[2]).removeClass("current");
         $($title3[0]).html($($title3[1]).text());
         $($day3[0]).html($($day3[1]).text());
         $($minute3[0]).html($($minute3[1]).text());
         $($price3[0]).html($($price3[1]).text());
         //repitable===
     } else if (value == 'the_title_value2') {
-        $('.type-button-link-1').removeClass("current");
-        $('.type-button-link-2').addClass("current");
-        $('.type-button-link-3').removeClass("current");
+        $($link[0]).removeClass("current");
+        $($link[1]).addClass("current");
+        $($link[2]).removeClass("current");
         $($title3[0]).html($($title3[2]).text());
         $($day3[0]).html($($day3[2]).text());
         $($minute3[0]).html($($minute3[2]).text());
         $($price3[0]).html($($price3[2]).text());
         //repitable===
     } else if (value == 'the_title_value3') {
-        $('.type-button-link-1').removeClass("current");
-        $('.type-button-link-2').removeClass("current");
-        $('.type-button-link-3').addClass("current");
+        $($link[0]).removeClass("current");
+        $($link[1]).removeClass("current");
+        $($link[2]).addClass("current");
          $($title3[0]).html($($title3[3]).text());
         $($day3[0]).html($($day3[3]).text());
         $($minute3[0]).html($($minute3[3]).text());
@@ -126,21 +127,21 @@ $($price3[0]).html($($price3[1]).text());
 
 $(document).ready(function () {
     //repitable===
-    $(".type-button-link-1").click(function () {
+    $($link[0]).click(function () {
         $($title3[0]).html($($title3[1]).text());
         $($day3[0]).html($($day3[1]).text());
         $($minute3[0]).html($($minute3[1]).text());
         $($price3[0]).html($($price3[1]).text());
     });
     //repitable===
-    $(".type-button-link-2").click(function () {
+    $($link[1]).click(function () {
         $($title3[0]).html($($title3[2]).text());
         $($day3[0]).html($($day3[2]).text());
         $($minute3[0]).html($($minute3[2]).text());
         $($price3[0]).html($($price3[2]).text());
     });
     //repitable===
-    $(".type-button-link-3").click(function () {
+    $($link[2]).click(function () {
         $($title3[0]).html($($title3[3]).text());
         $($day3[0]).html($($day3[3]).text());
         $($minute3[0]).html($($minute3[3]).text());
@@ -177,7 +178,6 @@ $(document).ready(function(){
   });
 });
 
-
-$(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
-});
+$(function(){
+			$('.image-link').viewbox();
+		});
